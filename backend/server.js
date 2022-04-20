c00f2692a85812d12aeb790ad31d35c86c49cf46
ClemlG <legoues.c@gmail.com>
2022-04-20 11:39:52 +0200

    add new back files

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.end('Voila la réponse du enieme serveur');
+});
+
+
+server.listen(process.env.PORT || 3000)
